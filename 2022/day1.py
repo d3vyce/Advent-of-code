8aa3e9fdebb4b2175a8540f6e0337bcba23a216e
Nicolas <nicolas.sudres@hotmail.fr>
2022-12-01 22:08:47 +0100

    clean + opti code

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,17 +1,15 @@
 # Advent of Code 2022, Day 1, d3vyce
 
-with open('input/day1.txt') as f:
-    file = [i for i in f.read().splitlines()]
-
 count = 0
 tab = []
 
-for line in file:
-    if line == '':
-        tab.append(count)
-        count = 0
-    else:
-        count += int(line)
+with open('input/day1.txt') as f:
+    for line in f.read().splitlines():
+        if line == '':
+            tab.append(count)
+            count = 0
+        else:
+            count += int(line)
 
 print(f'Answer 1: {max(tab)}')
 
